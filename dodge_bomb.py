2b9ccac67dcef24de8da04bc86f1e4c39d1265dd
rsuzuki <c0a2208689@edu.teu.ac.jp>
2023-04-25 14:32:15 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,12 +10,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    tmr = 0
+    
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), (10))
     bb_img.set_colorkey((0, 0, 0))
     x, y = random.randint(0, 1600), random.randint(0, 900)
-
+    vx, vy = +1, +1
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y
+    
+    tmr = 0
 
     while True:
         for event in pg.event.get():
@@ -25,7 +29,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x, y])
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+
 
         pg.display.update()
         clock.tick(1000)

aeddcc573afc08c23a9c3a15e0e6bcbddc40993d
rsuzuki <c0a2208689@edu.teu.ac.jp>
2023-04-25 15:41:08 +0900

    練習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,23 @@ delta = {
         }
 
 
+def check_bound(scr_rct:pg.Rect, obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかとん、または、爆弾SurfaceのRect
+    戻り値：横方向、縦方向のはみ出し判定結果（画面内；True/画面外/False）
+
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -26,9 +43,10 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), (10))
     bb_img.set_colorkey((0, 0, 0))
     x, y = random.randint(0, 1600), random.randint(0, 900)
-    vx, vy = +1, +1
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y
+    vx, vy = +1, +1
+
     
     tmr = 0
 
@@ -44,11 +62,20 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
 
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
 
 
